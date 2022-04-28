52cbf017ad68b0c610b9ceddca223e9f65fa726c
Adam La Morre <alamorre@gmail.com>
2022-04-27 17:29:03 -0700

    react-chat-engine-advanced implemented

--- frontend/src/chatsPage/index.js
+++ frontend/src/chatsPage/index.js
@@ -4,8 +4,21 @@ import { Context } from "../context";
 
 import Header from "./Header";
 
+import {
+  MultiChatWindow,
+  useMultiChatLogic,
+  MultiChatSocket,
+} from "react-chat-engine-advanced";
+const projectId = "70049943-b572-4372-9f3c-fbdeca940e0f";
+
 const ChatsPage = () => {
   const { user } = useContext(Context);
+
+  const chatProps = useMultiChatLogic(
+    projectId,
+    user.username,
+    user.hashed_password
+  );
 
   if (!user) {
     return <div />;
@@ -13,7 +26,18 @@ const ChatsPage = () => {
     return (
       <div>
         <Header />
-        {user.first_name}'s chats...
+
+        <div
+          style={{ height: "calc(100vh - 64px)", backgroundColor: "#e8e5f4" }}
+        >
+          <div className="bubble-1" />
+          <div className="bubble-2" />
+          <div className="bubble-3" />
+          <div className="bubble-4" />
+
+          <MultiChatSocket {...chatProps} />
+          <MultiChatWindow {...chatProps} />
+        </div>
       </div>
     );
   }
